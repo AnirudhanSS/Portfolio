f9f57abeb5c186736f26edd4dbe9618035601060
Anirudhan <anirudhan1221@gmail.com>
2024-09-18 19:26:28 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,119 @@
+export const projects = [
+  {
+    title: "To-Do List",
+    desc: "A to-do list website made with React JS, Tailwind CSS, TypeScript, Redux Toolkit, and more.Applied to organize your tasks with the following data: title, description, date, mark as complete and mark as important.",
+    tags: [
+      {
+        name: "NextTs",
+        color: "red-600",
+      },
+      {
+        name: "TailwindCSS",
+        color: "green-600",
+      },
+    ],
+    img: "tdl.webp",
+    link: "https://phenomenal-custard-5d9dcb.netlify.app/",
+    code: "https://github.com/code-cracked/plants-vs-zombies-api",
+  },
+  {
+    title: "Web-Chat-App",
+    desc: "An app made with NextJs to help each other communicate fastly and securely. We can also search for the friends with their username.Realtime updates on messages.",
+    tags: [
+      {
+        name: "NextJs",
+        color: "red-600",
+      },
+      {
+        name: "ChakraUI",
+        color: "green-600",
+      },
+      {
+        name: "SocketIO",
+        color: "yellow-600",
+      },
+      {
+        name: "MongoDB",
+        color: "blue-800",
+      },
+    ],
+    img: "webchatapp.webp",
+    link: "https://web-chat-app-brown.vercel.app",
+    code: "https://github.com/daniel-jebarson/web-chat-app",
+  },
+  {
+    title: "Exploiter(deprecated)",
+    desc: "This is the simple multi-featured discord bot which is in more than 90+ servers that resembles lords mobile as mini game. It contains the featiures of many bots such as dyno,carl-bot,mee6 etc.It helps to authorize the users, audit the messages and can also be used for other server developments and also for mini-games.  ",
+    tags: [
+      {
+        name: "Python",
+        color: "red-600",
+      },
+      {
+        name: "Discord.py",
+        color: "green-600",
+      },
+      {
+        name: "discord-interactions",
+        color: "yellow-600",
+      },
+      {
+        name: "Flask",
+        color: "blue-800",
+      },
+    ],
+    img: "exploiter.webp",
+    link: "https://drive.google.com/file/d/1npEOuOhQh-1zRiLB9fFn-6ORTwd1gZkl/view",
+    code: "https://github.com/daniel-jebarson/EXPLOITER",
+  },
+  {
+    title: "Stock Market",
+    desc: "Stock Market Website done with ReactJs as Frontend and Express as Backend. This websites gives practice for people those who want to start using stock market. It is based on realtime update of stock value.",
+    tags: [
+      {
+        name: "ReactJs",
+        color: "red-600",
+      },
+      {
+        name: "ExpressJs",
+        color: "green-600",
+      },
+      {
+        name: "MongoDB",
+        color: "blue-800",
+      },
+    ],
+    img: "stockmarket.webp",
+    link: "https://stock-market-seven.vercel.app/",
+    code: "https://github.com/daniel-jebarson/Stock-Market",
+  },
+  {
+    title: "You-Farm",
+    desc: "An app made with NextJs, Machine Learning and ExpressJs to help farmers to sell their crops without intermediate. It also suggests which crops are needed for the specific soil.",
+    tags: [
+      {
+        name: "NextJs",
+        color: "red-600",
+      },
+      {
+        name: "MaterialUI",
+        color: "green-600",
+      },
+      {
+        name: "ExpressJs",
+        color: "yellow-600",
+      },
+      {
+        name: "FireBase",
+        color: "blue-800",
+      },
+      {
+        name: "Machine Learning",
+        color: "orange-600",
+      },
+    ],
+    img: "youfarm.webp",
+    link: "https://github.com/code-cracked/you-farm",
+    code: "https://github.com/code-cracked/you-farm",
+  },
+];
